77c7dc25639fde7238f071ddf24633f791481ded
Adam H <adam@adam.local>
2013-08-27 12:11:07 -0400

    updating FacilityType

--- erp_work_effort/app/models/facility_type.rb
+++ erp_work_effort/app/models/facility_type.rb
@@ -1,3 +1,8 @@
 class FacilityType < ActiveRecord::Base
   attr_accessible :description, :external_identifer_source, :external_identifier, :internal_identifier, :lft, :parent_id, :rgt
+
+  acts_as_nested_set
+  include ErpTechSvcs::Utils::DefaultNestedSetMethods
+
+  has_many :facilities
 end
