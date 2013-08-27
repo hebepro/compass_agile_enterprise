d762563bd8466f1f6fc49acc3922acfd56f45ee8
Adam H <adam@adam.local>
2013-08-27 10:10:09 -0400

    a spec method to test that a model can acts_as_work_effort

--- erp_work_effort/spec/models/work_effort_spec.rb
+++ erp_work_effort/spec/models/work_effort_spec.rb
@@ -8,4 +8,22 @@ describe WorkEffort do
   it "can be saved successfully" do
     WorkEffort.create().should be_persisted
   end
+
+  it "a model can acts_as_work_effort" do
+    AuditLog.class_eval do 
+      acts_as_work_effort
+    end
+
+    al = AuditLog.new
+    al.description = 'test audit log'
+    al.party_id = 1
+    al.audit_log_type_id = 1
+    al.save
+
+    al.should be_an_instance_of(AuditLog)
+    al.should be_persisted
+
+    al.work_effort.should be_an_instance_of(WorkEffort)
+    al.work_effort.should be_persisted
+  end
 end
