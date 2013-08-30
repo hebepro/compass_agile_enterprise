8228c4f5ff77c6330c677d1ec018474cb575d494
Jason Fletcher <jfletch@gmail.com>
2013-08-30 00:25:00 -0400

    - Add transportation_routes, and migration (in erp_work_effort placeholder for now). - Add acts_as_routable which emulates a lot of the has_many_polymorphic behavior.

--- erp_work_effort/lib/erp_work_effort/engine.rb
+++ erp_work_effort/lib/erp_work_effort/engine.rb
@@ -4,6 +4,7 @@ module ErpWorkEffort
     
     ActiveSupport.on_load(:active_record) do
       include ErpWorkEffort::Extensions::ActiveRecord::ActsAsWorkEffort
+      include ErpWorkEffort::Extensions::ActiveRecord::ActsAsRoutable
     end
 
     ErpBaseErpSvcs.register_as_compass_ae_engine(config, self)
