a9811bd91b446a2a90222432e4ccc24af9bc27e8
unknown <russellfholmes@gmail.com>
2013-09-08 12:22:22 -0400

    updated codemirror

--- erp_app/public/javascripts/erp_app/desktop/applications/file_manager/module.js
+++ erp_app/public/javascripts/erp_app/desktop/applications/file_manager/module.js
@@ -1,121 +1,120 @@
-Ext.define("Compass.ErpApp.Desktop.Applications.FileManager",{
-  extend:"Ext.ux.desktop.Module",
-  id:'file_manager-win',
-  setWindowStatus : function(status){
-    this.win.setStatus(status);
-  },
-    
-  clearWindowStatus : function(){
-    this.win.clearStatus();
-  },
+Ext.define("Compass.ErpApp.Desktop.Applications.FileManager", {
+    extend: "Ext.ux.desktop.Module",
+    id: 'file_manager-win',
+    setWindowStatus: function (status) {
+        this.win.setStatus(status);
+    },
 
-  init : function(){
-    this.launcher = {
-      text: 'File Manager',
-      iconCls:'icon-folders',
-      handler : this.createWindow,
-      scope: this
-    };
-  },
-  createWindow : function(){
-    var self = this;
-    var desktop = this.app.getDesktop();
-    var win = desktop.getWindow('file_manager');
-    if(!win){
+    clearWindowStatus: function () {
+        this.win.clearStatus();
+    },
 
-      var contentCardPanel = new Ext.Panel({
-        layout:'card',
-        autoDestroy:true,
-        frame:false,
-        border:false,
-        region:'center'
-      });
-			
-      var fileTreePanel = new Compass.ErpApp.Shared.FileManagerTree({
-        allowDownload:true,
-        addViewContentsToContextMenu:true,
-        region:'west',
-        rootVisible:true,
-        containerScroll: true,
-        standardUploadUrl:'/erp_app/desktop/file_manager/base/upload_file',
-        border: false,
-        width: 250,
-        frame:true,
-        listeners:{
-          'showImage':function(fileManager, record){
-            contentCardPanel.removeAll(true);
-            contentCardPanel.add(Ext.create('Ext.panel.Panel',{
-              closable:true,
-              layout:'fit',
-              html: '<img src="/erp_app/desktop/file_manager/base/download_file/?path=' + record.data.id + '" />'
-            }));
-            contentCardPanel.getLayout().setActiveItem(0);
-            return false;
-          },
-          'contentLoaded':function(fileManager, record, content){
-            var path = record.data.id;
-            var fileType = path.split('.').pop();
+    init: function () {
+        this.launcher = {
+            text: 'File Manager',
+            iconCls: 'icon-folders',
+            handler: this.createWindow,
+            scope: this
+        };
+    },
+    createWindow: function () {
+        var self = this;
+        var desktop = this.app.getDesktop();
+        var win = desktop.getWindow('file_manager');
+        if (!win) {
 
-
-            contentCardPanel.removeAll(true);
-            contentCardPanel.add({
-              disableToolbar:!currentUser.hasRole('admin'),
-              xtype:'codemirror',
-              parser:fileType,
-              sourceCode:content,
-              listeners:{
-                'save':function(codeMirror, content){
-                  self.setWindowStatus('Saving...');
-                  Ext.Ajax.request({
-                    url: '/erp_app/desktop/file_manager/base/update_file',
-                    method: 'POST',
-                    params:{
-                      node:path,
-                      content:content
-                    },
-                    success: function(response) {
-                      var obj = Ext.decode(response.responseText);
-                      if(obj.success){
-                        self.clearWindowStatus();
-                      }
-                    },
-                    failure: function(response) {
-                      self.clearWindowStatus();
-                      Ext.Msg.alert('Error', 'Error saving content');
-                    }
-                  });
-                }
-              }
+            var contentCardPanel = new Ext.Panel({
+                layout: 'card',
+                autoDestroy: true,
+                frame: false,
+                border: false,
+                region: 'center'
             });
-            contentCardPanel.getLayout().setActiveItem(0);
-          }
-        }
-      }
-      );
 
-      win = desktop.createWindow({
-        id: 'file_manager',
-        title:'File Manager',
-        width:1000,
-        height:550,
-        autoDestroy:true,
-        iconCls: 'icon-folders',
-        shim:false,
-        animCollapse:false,
-        constrainHeader:true,
-        layout: 'border',
-        items:[fileTreePanel,contentCardPanel],
-        listeners:{
-          'destroy':function(){
-            fileTreePanel.destroy();
-            contentCardPanel.destroy();
-          }
-        }
-      });
+            var fileTreePanel = new Compass.ErpApp.Shared.FileManagerTree({
+                    allowDownload: true,
+                    addViewContentsToContextMenu: true,
+                    region: 'west',
+                    rootVisible: true,
+                    containerScroll: true,
+                    standardUploadUrl: '/erp_app/desktop/file_manager/base/upload_file',
+                    border: false,
+                    width: 250,
+                    frame: true,
+                    listeners: {
+                        'showImage': function (fileManager, record) {
+                            contentCardPanel.removeAll(true);
+                            contentCardPanel.add(Ext.create('Ext.panel.Panel', {
+                                closable: true,
+                                layout: 'fit',
+                                html: '<img src="/erp_app/desktop/file_manager/base/download_file/?path=' + record.data.id + '" />'
+                            }));
+                            contentCardPanel.getLayout().setActiveItem(0);
+                            return false;
+                        },
+                        'contentLoaded': function (fileManager, record, content) {
+                            var path = record.data.id,
+                                mode = Compass.ErpApp.Shared.CodeMirror.determineCodeMirrorMode(path);
 
-      this.win = win;
+                            contentCardPanel.removeAll(true);
+                            contentCardPanel.add({
+                                disableToolbar: !currentUser.hasRole('admin'),
+                                xtype: 'codemirror',
+                                mode: mode,
+                                sourceCode: content,
+                                listeners: {
+                                    'save': function (codeMirror, content) {
+                                        self.setWindowStatus('Saving...');
+                                        Ext.Ajax.request({
+                                            url: '/erp_app/desktop/file_manager/base/update_file',
+                                            method: 'POST',
+                                            params: {
+                                                node: path,
+                                                content: content
+                                            },
+                                            success: function (response) {
+                                                var obj = Ext.decode(response.responseText);
+                                                if (obj.success) {
+                                                    self.clearWindowStatus();
+                                                }
+                                            },
+                                            failure: function (response) {
+                                                self.clearWindowStatus();
+                                                Ext.Msg.alert('Error', 'Error saving content');
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                            contentCardPanel.getLayout().setActiveItem(0);
+                        }
+                    }
+                }
+            );
+
+            win = desktop.createWindow({
+                id: 'file_manager',
+                title: 'File Manager',
+                width: 1000,
+                height: 550,
+                autoDestroy: true,
+                iconCls: 'icon-folders',
+                shim: false,
+                animCollapse: false,
+                constrainHeader: true,
+                layout: 'border',
+                items: [fileTreePanel, contentCardPanel],
+                listeners: {
+                    'destroy': function () {
+                        fileTreePanel.destroy();
+                        contentCardPanel.destroy();
+                    }
+                }
+            });
+
+            this.win = win;
+        }
+        win.show();
+        return win;
     }
-    win.show();
-    return win;
-  }
 });
