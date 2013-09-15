5b03eea223719facf2a819621e94a8d8b7a895fe
Russell Holmes <russellfholmes@gmail.com>
2013-09-14 21:25:33 -0400

    added pixlr extensions

--- knitkit/public/javascripts/erp_app/desktop/applications/knitkit/image_assets_data_view.js
+++ knitkit/public/javascripts/erp_app/desktop/applications/knitkit/image_assets_data_view.js
@@ -1,31 +1,58 @@
-Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.ImageAssetsDataView",{
-  extend:"Ext.view.View",
-  alias:'widget.knitkit_imageassetsdataview',
-  
-  constructor : function(config) {
-    config = Ext.apply({
-      autoDestroy:true,
-      style:'overflow:auto',
-      itemSelector: 'div.thumb-wrap',
-      store: Ext.create('Ext.data.Store', {
-        proxy: {
-          type: 'ajax',
-          url: config['url'],
-          reader: {
-            type: 'json',
-            root: 'images'
-          }
-        },
-        fields:['name', 'url','shortName']
-      }),
-      tpl: new Ext.XTemplate(
-        '<tpl for=".">',
-        '<div class="thumb-wrap" id="{name}">',
-        '<div class="thumb"><img src="{url}" title="{name}" alt="{name}" class="thumb-img"></div>',
-        '<span>{shortName}</span></div>',
-        '</tpl>')
-    }, config);
+Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.ImageAssetsDataView", {
+    extend: "Ext.view.View",
+    alias: 'widget.knitkit_imageassetsdataview',
 
-    this.callParent([config]);
-  }
+    constructor: function (config) {
+        config = Ext.apply({
+            autoDestroy: true,
+            style: 'overflow:auto',
+            itemSelector: 'div.thumb-wrap',
+            store: Ext.create('Ext.data.Store', {
+                proxy: {
+                    type: 'ajax',
+                    url: config['url'],
+                    reader: {
+                        type: 'json',
+                        root: 'images'
+                    }
+                },
+                fields: ['name', 'url', 'shortName']
+            }),
+            tpl: new Ext.XTemplate(
+                '<tpl for=".">',
+                '<div class="thumb-wrap" id="{name}">',
+                '<div class="thumb"><img src="{url}" title="{name}" alt="{name}" class="thumb-img"></div>',
+                '<span>{shortName}</span></div>',
+                '</tpl>'),
+            listeners:{
+                'itemcontextmenu': function (view, record, htmlitem, index, e, options) {
+                    e.stopEvent();
+                    var contextMenu = Ext.create("Ext.menu.Menu", {
+                        items: [
+                            {
+                                text: 'Edit with Pixlr',
+                                iconCls: 'icon-picture',
+                                handler: function (btn) {
+                                    pixlr.overlay.show({
+                                        referrer: 'CompassAE',
+                                        exit: 'http://localhost:3000/pixlr/finish_edit',
+                                        image: 'http://1goat2chickens.com/sites/1goat2chickens/images/goat.png?1332205567',
+                                        title: record.get('name'),
+                                        method: 'GET',
+                                        locktarget: true,
+                                        locktitle: true,
+                                        locktype: true,
+                                        service: 'express'
+                                    })
+                                }
+                            }
+                        ]
+                    });
+                    contextMenu.showAt(e.xy);
+                }
+            }
+        }, config);
+
+        this.callParent([config]);
+    }
 });
