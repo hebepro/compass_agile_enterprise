0b25ba7bbce3ea9321e14df107967ba6875c3ffb
kristenwarren <kwarren@truenorthtechnology.com>
2014-02-04 11:15:15 -0500

    Fixed error in migration that adds sku, comment, uom_id to Product Type

--- erp_products/db/migrate/20140130211433_add_sku_comment_uom_to_product_type.rb
+++ erp_products/db/migrate/20140130211433_add_sku_comment_uom_to_product_type.rb
@@ -1,9 +1,7 @@
 class AddSkuCommentUomToProductType < ActiveRecord::Migration
   def change
-    def change
-      add_column :product_types, :sku, :string
-      add_column :product_types, :comment, :text
-      add_column :product_types, :unit_of_measurement_id, :integer
-    end
+    add_column :product_types, :sku, :string
+    add_column :product_types, :comment, :text
+    add_column :product_types, :unit_of_measurement_id, :integer
   end
 end
