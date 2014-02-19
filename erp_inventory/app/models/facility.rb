132255bf64f7d3c11663be0462742da71532180b
kristenwarren <kwarren@truenorthtechnology.com>
2014-02-19 10:11:57 -0500

    Added postal address association to facility and to_data_hash method

--- erp_inventory/app/models/facility.rb
+++ erp_inventory/app/models/facility.rb
@@ -3,4 +3,20 @@ class Facility < ActiveRecord::Base
   has_many :inventory_entry_locations
   has_many :inventory_entries, :through => :inventory_entry_locations
 
+  belongs_to :postal_address
+
+  def to_data_hash
+    {
+        :id => self.id,
+        :description => self.description,
+        :address_line_1 => self.postal_address.address_line_1,
+        :address_line_2 => self.postal_address.address_line_2,
+        :city => self.postal_address.city,
+        :state => self.postal_address.state,
+        :zip => self.postal_address.zip,
+        :created_at => self.created_at,
+        :updated_at => self.updated_at,
+    }
+  end
+
 end
