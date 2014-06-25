dc3bb45beed4db48db89664228e0525aaee117b3
Anup Biswal <anup.craver@gmail.com>
2014-06-25 21:36:21 +0530

    changed custom fields migrations to add hstore

--- erp_base_erp_svcs/db/migrate/20140401072612_add_custom_fields_to_party.rb
+++ erp_base_erp_svcs/db/migrate/20140401072612_add_custom_fields_to_party.rb
@@ -1,5 +1,10 @@
 class AddCustomFieldsToParty < ActiveRecord::Migration
   def change
-    add_column :parties, :custom_fields, :text
+    if ::ActiveRecord::Base.connection.instance_values["config"][:adapter] == 'postgresql'
+      add_column :parties, :custom_fields, :hstore
+      add_hstore_index :parties, :custom_fields
+    else
+      add_column :parties, :custom_fields, :text
+    end
   end
 end
