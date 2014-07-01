48a690b23286d9d6b1421237b27ef9bacf4afdcd
Russell Holmes <russellfholmes@gmail.com>
2014-07-01 16:49:12 -0400

    icon updates and minor bug fixes

--- knitkit/app/widgets/signup/javascript/signup.js
+++ knitkit/app/widgets/signup/javascript/signup.js
@@ -1,15 +1,13 @@
 Compass.ErpApp.Widgets.Signup = {
     template: new Ext.Template('<%= render_widget :signup, :params => {:login_url => "/login"}%>'),
-    addSignup:function(){
+    addSignup: function () {
         Ext.getCmp('knitkitCenterRegion').addContentToActiveCodeMirror(Compass.ErpApp.Widgets.Signup.template.apply());
     }
-}
+};
 
 Compass.ErpApp.Widgets.AvailableWidgets.push({
-    name:'Signup',
-    iconUrl:'/images/icons/user/user_48x48.png',
-    onClick:Compass.ErpApp.Widgets.Signup.addSignup,
-    about:'This widget allows new users of a website to sign up.'
+    name: 'Signup',
+    iconUrl: '/images/icons/sign_up/sign_up_48x48.png',
+    onClick: Compass.ErpApp.Widgets.Signup.addSignup,
+    about: 'This widget allows users to sign up.'
 });
-
-
