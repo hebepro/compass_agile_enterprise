5b6bc5b3558bfb49aaef92fbdff1c5d8620a5966
Russell Holmes <russellfholmes@gmail.com>
2014-10-28 12:17:39 -0400

    added check to see if it we are caching classes.

--- erp_base_erp_svcs/config/initializers/root_loader.rb
+++ erp_base_erp_svcs/config/initializers/root_loader.rb
@@ -1,3 +1,9 @@
-Rails.application.config.to_prepare do
-  ErpBaseErpSvcs.load_root_compass_ae_framework_extensions
+if Rails.application.config.cache_classes
+  Rails.application.config.after_initialize do
+    ErpBaseErpSvcs.load_root_compass_ae_framework_extensions
+  end
+else
+  Rails.application.config.to_prepare do
+    ErpBaseErpSvcs.load_root_compass_ae_framework_extensions
+  end
 end
