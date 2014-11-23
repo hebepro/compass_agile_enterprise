5b0e52a860af84394303d909a21934a096191a36
Russell Holmes <russellfholmes@gmail.com>
2014-11-23 15:40:24 -0500

    updated file_assets to support a many to many relationship

---
+++ erp_tech_svcs/app/models/file_asset_holder.rb
@@ -0,0 +1,19 @@
+# create_table :file_asset_holders do |t|
+#   t.references :file_asset
+#   t.references :file_asset_holder, :polymorphic => true
+#   t.text :scoped_by
+#
+#   t.timestamps
+# end
+#
+# add_index :file_asset_holders, :file_asset_id, :name => 'file_asset_holder_file_id_idx'
+# add_index :file_asset_holders, [:file_asset_holder_id, :file_asset_holder_type], :name => 'file_asset_holder_idx'
+
+class FileAssetHolder < ActiveRecord::Base
+
+  belongs_to :file_asset
+  belongs_to :file_asset_holder, :polymorphic => true
+
+  # setup scoping
+  add_scoped_by :scoped_by
+end
