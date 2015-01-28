5d593fecb44b1b37d8bc1e55a8ac23e415c0fe7b
Rick Koloski <rkoloski@tnsolutionsinc.com>
2015-01-28 11:02:42 -0500

    Splash updates

--- rails_db_admin/app/assets/javascripts/erp_app/desktop/applications/rails_db_admin/splash_screen.js
+++ rails_db_admin/app/assets/javascripts/erp_app/desktop/applications/rails_db_admin/splash_screen.js
@@ -2,130 +2,166 @@ Ext.define("Compass.ErpApp.Desktop.Applications.RailsDbAdmin.SplashScreen", {
     title: 'Startup',
     extend: "Ext.panel.Panel",
     alias: 'widget.railsdbadmin_splash_screen',
+    bodyStyle: 'background-color: #f2f6fa;',
     autoScroll: true,
     items: [
         {
-            html: "<div style='margin: 15px 50px 5px 50px;'><hr /><h2 style='margin: 3px 0px 0px 0px; color: #555; text-align: center'>Database Navigator</h2><hr /></div>"
+            html: "<div style='background-color: #f2f6fa; padding: 15px 50px 5px 50px;'><h2 style='margin: 3px 0px 0px 0px; color: #2fb3d4; text-align: center'>CompassAE Database Navigator</h2></div>"
         },
         {
             xtype: 'image',
-            style: 'padding-left: 50%; margin-left: -400px;',
-            src: '/assets/splash/splash.png'   // src: 'http://placehold.it/800x250'
+            style: 'background-color: #f2f6fa; padding-left: 50%; margin-top: 20px; margin-left: -150px;',
+            src: '/assets/splash/db_admin_splash.png'
         },
         {
-            html: "<div style='margin: 15px 50px 5px 50px;'><p style='margin: 0px; color: #222; text-align: center; font-size: 16px; font-weight: 300;'>Click on the shortcuts below to get started.</p></div>"
+            html: "<div style='background-color: #f2f6fa; padding: 15px 50px 5px 35px;'><p style='margin: 0px 0px 10px 5px; color: #222; text-align: center; font-size: 16px; font-weight: 300;'>Click on the shortcuts below to get started.</p></div>"
         },
         {
             xtype: 'panel',
             layout: 'column',
-            style: 'width: 480px; margin-top: 0px; margin-left: auto; margin-right: auto;',
+            style: 'background-color: #f2f6fa; width: 450px; margin-top: 0px; margin-left: auto; margin-right: auto;',
             items: [
 
                 {
                     xtype: 'panel',
                     height: 140,
-                    width: 140,
+                    width: 150,
                     style:{
-                        margin: '0px 0px 5px 10px;',
+                        margin: '',
                         cursor: 'pointer'
                     },
-                    bodyStyle: 'background: #ddd; padding: 20px; border-radius: 7px; border-color: #aaa !important;',
-                    border: true,
-                    bodyBorder: true,
+                    bodyStyle: 'background-color: #f2f6fa;',
+                    border: false,
+                    bodyBorder: false,
                     overCls: 'shortcut-hover',
                     items: [
                         {
                             xtype: 'image',
-                            src: '/assets/splash/images/data-model-icon.png',
-                            height: 80,
-                            width: 80,
-                            style: 'margin: 0px 0px 5px 10px;',
+                            src: '/assets/splash/icons/console_icon.png',
+                            height: 115,
+                            width: 115,
+                            style: 'background-color: #f2f6fa; margin-left: 15px;',
                             listeners: {
                                 render: function (component) {
                                     component.getEl().on('click', function (e) {
-                                        var module = compassDesktop.getModule('rails_db_admin-win');
 
-                                        module.setWindowStatus('Retrieving Docs...');
-                                        module.openIframeInTab('Data Models', 'http://documentation.compassagile.com');
-                                        module.clearWindowStatus();
+                                        var cr = this.findParentByType('applicationmanagementcenterregion');
+
+                                        cr.setWindowStatus('Opening CompassAE Console...');
+                                        cr.addConsolePanel();
+                                        cr.clearWindowStatus();
 
                                     }, component);
+
+                                    component.getEl().on('mouseover', function () {
+                                            component.setSrc('/assets/splash/icons/console_icon_active.png');
+                                        }
+                                    );
+                                    component.getEl().on('mouseout', function () {
+                                            component.setSrc('/assets/splash/icons/console_icon.png');
+                                        }
+                                    );
                                 }
                             }
                         },
                         {
-                            html: "<p style='background-color: #ddd; margin: 0px; text-align: center'>Browse the DB models</p>"
+                            html: "<p style='background-color: #f2f6fa; margin: 0px; text-align: center'>Open a console</p>"
+                        }
+                    ]
+                },
+
+                {
+                    xtype: 'panel',
+                    height: 140,
+                    width: 150,
+                    style:{
+                        margin: '',
+                        cursor: 'pointer'
+                    },
+                    bodyStyle: 'background-color: #f2f6fa;',
+                    border: false,
+                    bodyBorder: false,
+                    overCls: 'shortcut-hover',
+                    items: [
+                        {
+                            xtype: 'image',
+                            src: '/assets/splash/icons/db_icon.png',
+                            height: 115,
+                            width: 115,
+                            style: 'background-color: #f2f6fa; margin-left: 15px;',
+                            listeners: {
+                                render: function (component) {
+                                    component.getEl().on('click', function (e) {
+
+                                        var cr = this.findParentByType('applicationmanagementcenterregion');
+
+                                        cr.setWindowStatus('Opening CompassAE Console...');
+                                        cr.addConsolePanel();
+                                        cr.clearWindowStatus();
+
+                                    }, component);
+
+                                    component.getEl().on('mouseover', function () {
+                                            component.setSrc('/assets/splash/icons/db_icon_active.png');
+                                        }
+                                    );
+                                    component.getEl().on('mouseout', function () {
+                                            component.setSrc('/assets/splash/icons/db_icon.png');
+                                        }
+                                    );
+                                }
+                            }
+                        },
+                        {
+                            html: "<p style='background-color: #f2f6fa; margin: 0px; text-align: center'>Browse the DB</p>"
                         }
                     ]
                 },
                 {
                     xtype: 'panel',
                     height: 140,
-                    width: 140,
+                    width: 150,
                     style:{
-                        margin: '0px 0px 5px 10px;',
+                        margin: '',
                         cursor: 'pointer'
                     },
-                    bodyStyle: 'background: #ddd; padding: 20px; border-radius: 7px; border-color: #aaa !important;',
-                    border: true,
-                    bodyBorder: true,
+                    bodyStyle: 'background-color: #f2f6fa;',
+                    border: false,
+                    bodyBorder: false,
                     overCls: 'shortcut-hover',
                     items: [
                         {
                             xtype: 'image',
-                            src: '/assets/splash/images/console-icon.png',
-                            height: 80,
-                            width: 80,
-                            style: 'margin: 0px 0px 5px 10px;',
+                            src: '/assets/splash/icons/help_icon.png',
+                            height: 115,
+                            width: 115,
+                            style: 'background-color: #f2f6fa; margin-left: 15px;',
                             listeners: {
                                 render: function (component) {
                                     component.getEl().on('click', function (e) {
-                                        var module = compassDesktop.getModule('rails_db_admin-win');
 
-                                        module.addConsolePanel();
+                                        var cr = this.findParentByType('applicationmanagementcenterregion');
+
+                                        cr.setWindowStatus('Opening CompassAE Console...');
+                                        cr.addConsolePanel();
+                                        cr.clearWindowStatus();
+
                                     }, component);
+
+                                    component.getEl().on('mouseover', function () {
+                                            component.setSrc('/assets/splash/icons/help_icon_active.png');
+                                        }
+                                    );
+                                    component.getEl().on('mouseout', function () {
+                                            component.setSrc('/assets/splash/icons/help_icon.png');
+                                        }
+                                    );
+
                                 }
                             }
                         },
                         {
-                            html: "<p style='background-color: #ddd; margin: 0px; text-align: center'>Open a console</p>"
-                        }
-                    ]
-                },
-                {
-                    xtype: 'panel',
-                    height: 140,
-                    width: 140,
-                    style:{
-                        margin: '0px 0px 5px 10px;',
-                        cursor: 'pointer'
-                    },
-                    bodyStyle: 'background: #ddd; padding: 20px; border-radius: 7px; border-color: #aaa !important;',
-                    border: true,
-                    bodyBorder: true,
-                    overCls: 'shortcut-hover',
-                    items: [
-                        {
-                            xtype: 'image',
-                            src: '/assets/knitkit/splash/images/tutorials.png',
-                            height: 80,
-                            width: 80,
-                            style: 'margin: 0px 0px 5px 10px;',
-                            listeners: {
-                                render: function (component) {
-                                    component.getEl().on('click', function (e) {
-                                        var module = compassDesktop.getModule('rails_db_admin-win');
-
-                                        module.setWindowStatus('Retrieving Docs...');
-                                        module.openIframeInTab('Tutorials', 'http://tutorials.compassagile.com');
-                                        module.clearWindowStatus();
-
-                                    }, component);
-                                }
-                            }
-                        },
-                        {
-                            html: "<p style='background-color: #ddd; margin: 0px; text-align: center'>Learn More!</p>"
+                            html: "<p style='background-color: #f2f6fa; margin: 0px; text-align: center'>Learn more</p>"
                         }
                     ]
                 }
