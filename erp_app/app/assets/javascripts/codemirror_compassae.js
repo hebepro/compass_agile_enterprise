85181afdfffd7b6c541d4e0856c2fa0b766ff172
Russell Holmes <russellfholmes@gmail.com>
2015-02-23 12:31:00 -0500

    upgraded codemirror

---
+++ erp_app/app/assets/javascripts/codemirror_compassae.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require codemirror
+//= require codemirror/modes/ruby
+//= require codemirror/modes/css
+//= require codemirror/modes/javascript
+//= require codemirror/modes/sql
+//= require codemirror/modes/yaml
+//= require codemirror/modes/markdown
+//= require codemirror/modes/htmlmixed
+//= require codemirror/modes/xml
+//= require codemirror/modes/htmlembedded
+//= require codemirror/addons/fold/foldcode
+//= require codemirror/addons/fold/foldgutter
+//= require codemirror/addons/fold/brace-fold
+//= require codemirror/addons/fold/comment-fold
+//= require codemirror/addons/fold/indent-fold
+//= require codemirror/addons/fold/markdown-fold
+//= require codemirror/addons/fold/xml-fold
--- erp_base_erp_svcs/app/models/generated_item.rb
+++ erp_base_erp_svcs/app/models/generated_item.rb
@@ -1,3 +1,13 @@
+# create_table :generated_items do |t|
+#   t.references :generated_by, :polymorphic => true
+#   t.references :generated_record, :polymorphic => true
+#
+#   t.timestamps
+# end
+#
+# add_index :generated_items, [:generated_by_type, :generated_by_id], :name => 'generated_by_idx'
+# add_index :generated_items, [:generated_record_type, :generated_record_id], :name => 'generated_record_idx'
+
 class GeneratedItem < ActiveRecord::Base
   attr_protected :created_at, :updated_at
 
