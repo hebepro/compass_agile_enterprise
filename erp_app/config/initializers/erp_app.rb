8e4bb123fd743ebcf6ab2756fa3714997ea076ff
Russell Holmes <russellfholmes@gmail.com>
2015-03-23 17:02:40 -0400

    removed initializer as it is already in config

--- erp_app/config/initializers/erp_app.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.erp_app.configure do |config|
-  config.session_warn_after = 18 # in minutes
-  config.session_redirect_after = 20 # in minutes
-  config.max_js_loader_order_index = 9999 # max loader order index for a js file
-end
-Rails.application.config.erp_app.configure!
