2c348fb18195f688333b5a68c6d03ddf9ddd37e1
Jim Laskowski <jlaskowski@truenorthtechnology.com>
2015-04-06 00:15:55 -0400

    - continued clean up to remove positions, shifts, and wc code fields - moved Party's has_many for wc_codes and shifts to erp_work_effort where these models are created

--- erp_orders/app/models/staffing_requisition.rb
+++ erp_orders/app/models/staffing_requisition.rb
@@ -1,16 +1,4 @@
 class StaffingRequisition < ActiveRecord::Base
-  # create_table :staffing_requisitions do |t|
-  #   t.integer :parent_id
-  #   t.string :description
-  #   t.string :internal_identifier
-  #   t.string :shift
-  #   t.decimal :rate, :precision => 8, :scale => 2
-  #   t.integer :staffing_position_id
-  #   t.date :expected_start
-  #   t.date :expected_end
-  #   t.integer :quantity
-  #   t.text :custom_fields
-  #   t.timestamps
 
   attr_accessible :parent_id,
                   :description,
@@ -18,7 +6,5 @@ class StaffingRequisition < ActiveRecord::Base
                   :expected_end,
                   :expected_start,
                   :quantity,
-                  :rate,
-                  :shift,
-                  :staffing_position_id
+                  :rate
 end
