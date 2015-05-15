803c72ede0dde8813f3a9c0cac8ce3da3b601d9e
Gerry Boyd <tealboyd@gmail.com>
2015-05-15 07:53:32 -0400

    AE-172: Fixed migrations to include pre-existing checks

--- erp_orders/db/migrate/20150311160005_add_unit_price_to_order_line_items.rb
+++ erp_orders/db/migrate/20150311160005_add_unit_price_to_order_line_items.rb
@@ -1,5 +1,5 @@
 class AddUnitPriceToOrderLineItems < ActiveRecord::Migration
   def change
-    add_column :order_line_items, :unit_price, :decimal
+    add_column :order_line_items, :unit_price, :decimal unless column_exists?(table, :unit_price)
   end
 end
