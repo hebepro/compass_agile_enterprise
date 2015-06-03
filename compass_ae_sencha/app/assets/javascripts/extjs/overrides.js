a567e6b2b98eea5e69cfa7f7fcaa72944420b43c
Russell Holmes <russellfholmes@gmail.com>
2015-06-03 14:41:19 -0400

    fixed bug introduced by Chrome 43 that is hiding submenus in ExtJs

--- compass_ae_sencha/app/assets/javascripts/extjs/overrides.js
+++ compass_ae_sencha/app/assets/javascripts/extjs/overrides.js
@@ -1,55 +1,84 @@
 Ext.override(Ext.data.Store, {
-  setExtraParam: function (name, value){
-    this.proxy.extraParams = this.proxy.extraParams || {};
-    this.proxy.extraParams[name] = value;
-    this.proxy.applyEncoding(this.proxy.extraParams);
-  }
+    setExtraParam: function (name, value) {
+        this.proxy.extraParams = this.proxy.extraParams || {};
+        this.proxy.extraParams[name] = value;
+        this.proxy.applyEncoding(this.proxy.extraParams);
+    }
 });
 
 // fix for tempHidden in ExtJS 4.0.7 - Invoice Mgmt window was not opening correctly
 // taken from http://www.sencha.com/forum/showthread.php?160222-quot-this.tempHidden-is-undefined-quot-Error-Workaround
 Ext.override(Ext.ZIndexManager, {
-  tempHidden: [],
+    tempHidden: [],
 
-  show: function() {
-    var comp, x, y;
+    show: function () {
+        var comp, x, y;
 
-    while (comp = this.tempHidden.shift()) {
-      x = comp.x;
-      y = comp.y;
+        while (comp = this.tempHidden.shift()) {
+            x = comp.x;
+            y = comp.y;
 
-      comp.show();
-      comp.setPosition(x, y);
+            comp.show();
+            comp.setPosition(x, y);
+        }
     }
-  }
 });
 
 Ext.define('Compass.ErpApp.Shared.RowEditingOverride', {
-  override: 'Ext.grid.plugin.RowEditing',
-  startEdit: function (record, columnHeader) {
-    var me = this,
-        editor = me.getEditor(),
-        context;
+    override: 'Ext.grid.plugin.RowEditing',
+    startEdit: function (record, columnHeader) {
+        var me = this,
+            editor = me.getEditor(),
+            context;
 
-    if (Ext.isEmpty(columnHeader)) {
-      columnHeader = me.grid.getTopLevelVisibleColumnManager().getHeaderAtIndex(0);
-    }
-
-    if (editor.beforeEdit() !== false) {
-      context = me.callSuper([record, columnHeader]);
-      if (context) {
-        me.context = context;
-
-        // If editing one side of a lockable grid, cancel any edit on the other side.
-        if (me.lockingPartner) {
-          me.lockingPartner.cancelEdit();
+        if (Ext.isEmpty(columnHeader)) {
+            columnHeader = me.grid.getTopLevelVisibleColumnManager().getHeaderAtIndex(0);
         }
-        editor.startEdit(context.record, context.column, context);
-        me.fireEvent('editstarted', editor);
-        me.editing = true;
-        return true;
-      }
+
+        if (editor.beforeEdit() !== false) {
+            context = me.callSuper([record, columnHeader]);
+            if (context) {
+                me.context = context;
+
+                // If editing one side of a lockable grid, cancel any edit on the other side.
+                if (me.lockingPartner) {
+                    me.lockingPartner.cancelEdit();
+                }
+                editor.startEdit(context.record, context.column, context);
+                me.fireEvent('editstarted', editor);
+                me.editing = true;
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-  }
+});
+
+// fix hide submenu (in chrome 43)
+Ext.override(Ext.menu.Menu, {
+    onMouseLeave: function (e) {
+        var me = this;
+
+        // BEGIN FIX
+        var visibleSubmenu = false;
+        me.items.each(function (item) {
+            if (item.menu && item.menu.isVisible()) {
+                visibleSubmenu = true;
+            }
+        });
+
+        if (visibleSubmenu) {
+            //console.log('apply fix hide submenu');
+            return;
+        }
+        // END FIX
+
+        me.deactivateActiveItem();
+
+        if (me.disabled) {
+            return;
+        }
+
+        me.fireEvent('mouseleave', me, e);
+    }
 });
